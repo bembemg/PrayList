62c1b181bd85f4fbd23a0210f3f527196a16a924
Gabriel Bembem <92057057+bembemg@users.noreply.github.com>
2025-02-05 23:35:23 -0300

    PORTCHANGE

--- server/index.js
+++ server/index.js
@@ -111,8 +111,8 @@ app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT_INDEX = process.env.PORT_INDEX || 3001;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT_INDEX, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
